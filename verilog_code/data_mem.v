a12aadb6f80773f0b9f9c1cb6c046daa04100667
CastleFirstKR <154411859+CastleFirstKR@users.noreply.github.com>
2024-01-30 22:04:49 +0900

    Update data_mem.v

--- verilog_code/data_mem.v
+++ verilog_code/data_mem.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/06/2023 04:14:11 PM
-// Design Name: 
-// Module Name: data_mem
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 module data_mem(
 
 	input	wire		clk,
