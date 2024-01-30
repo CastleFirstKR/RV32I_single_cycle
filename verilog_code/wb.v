ef8e7ca0a4a4a09d82dcf73fa857bfdcdf6377b3
CastleFirstKR <154411859+CastleFirstKR@users.noreply.github.com>
2024-01-30 22:05:27 +0900

    Update wb.v

--- verilog_code/wb.v
+++ verilog_code/wb.v
@@ -1,27 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/06/2023 03:52:02 PM
-// Design Name: 
-// Module Name: wb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-
-
 module WB (
 
     input   wire        rst,
