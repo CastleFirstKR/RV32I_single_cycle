727c8f4e3e553a329eb688128714331b8386106e
CastleFirstKR <154411859+CastleFirstKR@users.noreply.github.com>
2024-01-30 22:05:01 +0900

    Update ins_mem.v

--- verilog_code/ins_mem.v
+++ verilog_code/ins_mem.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/04/2023 11:47:58 PM
-// Design Name: 
-// Module Name: ins_mem
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 module inst_mem(
 
 	input	wire		rst,    // chip select signal
