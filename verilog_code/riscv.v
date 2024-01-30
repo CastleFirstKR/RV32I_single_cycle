53363f9177104a931777e7810cd8cafbe8c8272a
CastleFirstKR <154411859+CastleFirstKR@users.noreply.github.com>
2024-01-30 22:05:12 +0900

    Update riscv.v

--- verilog_code/riscv.v
+++ verilog_code/riscv.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/06/2023 04:03:53 PM
-// Design Name: 
-// Module Name: riscv
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module riscv(
 
